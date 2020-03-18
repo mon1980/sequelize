8b4ffc1c134a8c65c049c5f611fd5f369244a174
Monica <mcg_8@hotmail.com>
2020-03-18 10:26:44 +0100

    First commit

---
+++ backend/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const morgan= require('morgan');
+const app = express();
+const PORT = 3000;
+app.use(morgan('dev'));
+app.use(express.json()); // para evitar req.body sea undefined
+app.listen(PORT, () => console.log('server running on ' + PORT));
